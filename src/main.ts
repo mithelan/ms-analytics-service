661e31ae204553eb8078106d4f29ac0fd57c4980
mithelan <mithelandev@gmail.com>
2025-06-24 23:28:29 +0530

    change to main

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3001;
-
+  
+  app.enableCors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  });
   try {
     await app.listen(PORT, '0.0.0.0');
     console.log(`✅ App is running on http://localhost:${PORT}`);
