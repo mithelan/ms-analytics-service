51ee0962c9363bd76d08c78a98274f4601c8f9e5
mithelan <mithelandev@gmail.com>
2025-07-17 22:51:48 +0530

    blue-green

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,7 +9,7 @@ export class AnalyticsController {
 
   @Get('/health')
   health() {
-    return { status: 'Ok 16.01', env: process.env.NODE_ENV, clickHouse: process.env.CLICK_HOUSE_USERNAME, service: 'Analytics' };
+    return { status: 'Ok 16.01', env: process.env.NODE_ENV, clickHouse: process.env.CLICK_HOUSE_USERNAME, service: 'Analytics' , version : process.env.APP_VERSION};
   }
 
   @Post('analytics/create')
